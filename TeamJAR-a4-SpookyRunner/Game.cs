d1b9b9da23fa1fbe839026cedc72244a90446da1
Jamie <Jamie@DESKTOP-UBOP17V>
2024-11-19 19:21:06 -0500

    add Window Size, Add Window Title

--- TeamJAR-a4-SpookyRunner/Game.cs
+++ TeamJAR-a4-SpookyRunner/Game.cs
@@ -18,7 +18,8 @@ namespace Game10003
         /// </summary>
         public void Setup()
         {
-
+            Window.SetSize(800, 600);
+            Window.SetTitle("Spooky Runner");
         }
 
         /// <summary>
