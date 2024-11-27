3a4d0afa78c6c8ca803f166c45c9e394a0cde493
Jamie <Jamie@DESKTOP-UBOP17V>
2024-11-27 15:49:44 -0500

    fixedbug on here

--- TeamJAR-a4-SpookyRunner/collision.cs
+++ TeamJAR-a4-SpookyRunner/collision.cs
@@ -2,27 +2,28 @@
 using System.Numerics;
 using System.Security.Cryptography.X509Certificates;
 
-public class collision
-{
+
 	public class collision()
-	{
-		public void Colllsion()
-		{ Vector2 position = new Vector2(0, 400);
-            Vector2 size = new Vector2(800, 200);
-            Vector2 point = new Vector2(40, 360);
-            float radius = 40;
-            Vector2 gravity = new Vector2(0, 10);
-            Vector2 speed = new Vector2(0, 0);
-            Vector2 obstaclePosition = new Vector2(650, 350);
-            Vector2 obstacleSpeed = new Vector2(-50, 0);
-            Vector2 obstacleGravity = new Vector2(0, 0);
-            public static bool End = false;
+    {
+        Vector2 position = new Vector2(0, 400);
+        Vector2 size = new Vector2(800, 200);
+        Vector2 point = new Vector2(40, 360);
+        float radius = 40;
+        Vector2 gravity = new Vector2(0, 10);
+        Vector2 speed = new Vector2(0, 0);
+        Vector2 obstaclePosition = new Vector2(650, 350);
+        Vector2 obstacleSpeed = new Vector2(-50, 0);
+        Vector2 obstacleGravity = new Vector2(0, 0);
+        public static bool End = false;
+        public void Colllsion()
+		{
 		}
 		public void Setup()
-		{
-            
+		{      
         Window.TargetFPS = 60;
             Window.SetSize(800, 600);
+           
+       
     }
 		public void Update()
         {
@@ -77,4 +78,4 @@ public class collision
     }
 
     }
-}
+
