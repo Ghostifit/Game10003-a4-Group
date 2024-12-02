0517c7429789fc820c7d4ba36bc67cf6b9bf1503
Jamie <Jamie@DESKTOP-UBOP17V>
2024-12-01 21:09:11 -0500

    Change name of Audio class to "Sound" add sound asset folder, add path for audios in sound.cs

---
+++ TeamJAR-a4-SpookyRunner/Game10003/Sound.cs
@@ -0,0 +1,36 @@
+﻿using Game10003;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamJAR_a4_SpookyRunner.Game10003
+{
+    public class sound
+    {
+        Music myMusic;
+        Sound JumpSound;
+        Sound ZombieSound;
+        Sound BatSound;
+
+
+        void setup()
+        {
+            //Audio for background music
+            myMusic = Audio.LoadMusic("../../../SoundAssets/BackgroundMusic.mp3");
+
+            //Audio for jump sound effect
+            JumpSound = Audio.LoadSound("../../../SoundAssets/Jump.mp3");
+            //Audio for Zombie sound effect
+            ZombieSound = Audio.LoadSound("../../../SoundAssets/Zombie.mp3");
+            //Audio for Bat sound effect
+            BatSound = Audio.LoadSound("../../../SoundAssets/Bat.mp3");
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
