199c711b66bf860acd04e304c0316d69ab7ad739
catlo <catlo@RILEYSLAPTOP>
2024-12-01 21:49:23 -0500

    Add sprites branch

--- TeamJAR-a4-SpookyRunner/Game.cs
+++ TeamJAR-a4-SpookyRunner/Game.cs
@@ -1,6 +1,8 @@
 ﻿// Include code libraries you need below (use the namespace).
 using System;
+using System.IO;
 using System.Numerics;
+using System.Text.RegularExpressions;
 
 // The namespace your code is in.
 namespace Game10003
@@ -12,15 +14,17 @@ namespace Game10003
     {
         // Place your variables here:
         collision collision = new collision();
+        string dir = System.IO.Directory.GetCurrentDirectory();
+        Texture2D moon;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
-        {
+        {    
             Window.SetSize(800, 600);
             Window.SetTitle("Spooky Runner");
-            
+            moon = Graphics.LoadTexture("../../../Sprites/Moon.png");
         }
 
         /// <summary>
@@ -28,8 +32,9 @@ namespace Game10003
         /// </summary>
         public void Update()
         {   Window.ClearBackground(Color.Gray);
-                collision.Update();  
-
+            collision.Update();
+            Graphics.Scale = 1;
+            Graphics.Draw(moon, 500, 0);
         }
     }
 }
---
+++ TeamJAR-a4-SpookyRunner/sprites.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamJAR_a4_SpookyRunner
+{
+    internal class sprites
+    {
+    public void Setup()
+        {
+
+        }
+
+    public void Update()
+        {
+
+        }
+    }
+
+}
